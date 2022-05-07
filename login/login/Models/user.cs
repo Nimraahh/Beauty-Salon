29dbba3533ebfebaa28aec7b90509ab8ed9ac209
Nimra Abid <bsef19a022@pucit.edu.pk>
2022-05-07 23:41:10 +0500

    1st commit

---
+++ login/login/Models/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace login.Models
+{
+    public class user
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+
+    }
+}
